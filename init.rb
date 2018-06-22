eb9f5f52c92e76652436e9373b447b6870f6c2f2
Carlos Meyer <cm@davitec.de>
2018-06-22 10:05:17 +0200

    inital

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :dv_open_issue_count_project_view do
+  name 'Dv Open Issue Count Project View plugin'
+  author 'Carlos Meyer'
+  description 'Shows open issue count on project view page'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
